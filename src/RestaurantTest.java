3a203cd4c1d195f31f75191d86630e4d973acf3d
jujpenabe <jujpenabe@unal.edu.co>
2020-05-12 20:00:19 -0500

    Finish Test qith get drink beer

--- src/RestaurantTest.java
+++ src/RestaurantTest.java
@@ -2,7 +2,7 @@ public class Restaurantest {
 
     public static void main(String[] args) {
         RestaurantAbstractFactory Factory = new RestaurantFactoryProducer().getRestaurantAbstractFactory(1);
-        Drink soda = Factory.getDrink("soda");
-        System.out.println(soda);
+        Drink soda = Factory.getDrink("beer");
+        System.out.println(beer);
     }
 }
