83967c47b246d2f84d7131c5da48332847d565de
jujpenabe <jujpenabe@unal.edu.co>
2020-05-12 19:27:28 -0500

    Create Sandwich.java

---
+++ src/Sandwich.java
@@ -0,0 +1,11 @@
+  
+public class Sandwich implements FastFood{
+    @Override
+    public void prepareIngredients() {}
+
+    @Override
+    public void cook() {}
+
+    @Override
+    public void serve() {}
+}
